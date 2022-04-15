c31ab23e29ef5c6b9c6b144fdd7dbdf46b738fd6
Mohamed Sami <srmf2025@gmail.com>
2022-04-15 14:57:59 +0530

    add logger middleware

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const logger = require("./middleware/logger");
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+app.use(logger);
 
 const PORT = process.env.PORT || 3000;
 
